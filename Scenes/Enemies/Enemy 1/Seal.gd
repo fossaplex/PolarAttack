980df3336f30e06d811602057d58ac355c83d3ab
ekareem <emmanuel45kareem@gmail.com>
2024-04-06 19:52:37 -0400

    PA-7 player movement and y sort

--- Scenes/Enemies/Enemy 1/Seal.gd
+++ Scenes/Enemies/Enemy 1/Seal.gd
@@ -1,14 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+@export var SPEED = 20.0
 
-var PLAYER
-
-func _ready():
-	PLAYER = get_node('/root/Level/Player')
+@onready var target: CharacterBody2D = get_node('/root/Level/Player')
 
 func _physics_process(delta):
-	#var direction = global_position.direction_to(PLAYER.global_position)
-	#var velocity = direction * SPEED 
-	position += (PLAYER.position - position)/SPEED
+	var direction = (target.global_position - global_position)
+	var direction_normalize = direction.normalized()
+	var target_distance = global_position.distance_to(target.global_position)
+	velocity = direction_normalize * SPEED if target_distance > 10 else Vector2.ZERO
 	move_and_slide()
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 
-func _process(delta):
+func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * SPEED
 
