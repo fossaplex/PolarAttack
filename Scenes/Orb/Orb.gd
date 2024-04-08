5ab1d2629a83b5819c6b08cf508a3df244644405
Mark <marklmiller2010@gmail.com>
2024-04-08 17:32:53 -0400

    Added orb buz sound + spatial DB

--- Scenes/Orb/Orb.gd
+++ Scenes/Orb/Orb.gd
@@ -18,16 +18,21 @@ func _ready():
 
 func _process(delta):
 	# Update the current angle based on the orbit speed
+	
 	orbit_angle += orbit_speed * delta
 	# Ensure the angle wraps around correctly (optional, keeps the number manageable)
 	orbit_angle = fmod(orbit_angle,2 * PI)
 	var time = Time.get_ticks_msec()
 	var orbit_radius = base_orbit_radius + sin(time * wave_speed) * orbit_radius_variation
+	var max_orbit_radius = base_orbit_radius + orbit_radius_variation
+	$AudioStreamPlayer2D.volume_db = linear_to_db(1 - orbit_radius / max_orbit_radius)
+	$AudioStreamPlayer2D.pitch_scale = 1.0 + (max_orbit_radius - orbit_radius) / max_orbit_radius
+
 	# Calculate the new position using trigonometry
 	var x = cos(orbit_angle) * orbit_radius
 	var y = sin(orbit_angle) * orbit_radius
 	
-	# Update the position of the Area2D to orbit around the player
+	# Update the position of the Area2D to orbit arou nd the player
 	position = Vector2(x, y)
 
 func _on_body_entered(body: Node2D):
@@ -44,7 +49,7 @@ func _on_area_entered(area: Area2D):
 	var parent = area.get_parent()
 	if parent.is_in_group("enemies"):
 		parent.queue_free() 
-		hearts.subtract_health(1)
+		hearts.subtract_health(1) 
 		var audio_player = get_parent().get_node("SoundKill")
 		if audio_player:
 			audio_player.play()
