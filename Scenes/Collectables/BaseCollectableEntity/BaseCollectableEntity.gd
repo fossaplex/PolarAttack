eefd69733a3a31634cfd53309b708689113ba5e2
grantrawlins <rawlinsgrant@yahoo.com>
2024-04-10 21:45:05 -0400

    initial creation of xp
    I have player collecting xp working now I need to make it so enemies drop xp

---
+++ Scenes/Collectables/BaseCollectableEntity/BaseCollectableEntity.gd
@@ -0,0 +1,20 @@
+extends Area2D
+class_name BaseCollectableEntity
+
+@export var collectable_resource : BaseCollectableResource
+
+@onready var sprite_2d = $Sprite2D
+
+
+signal collect_entity
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("collect_entity", on_collect)
+	sprite_2d.texture = collectable_resource.collectable_texture
+	
+func on_collect() -> void:
+	SignalBus.emit_collect_entity(collectable_resource)
+	queue_free()
+	
--- autoloads/SignalBus.gd
+++ autoloads/SignalBus.gd
@@ -1,4 +1,8 @@
 extends Node
 
+signal collect_entity(collectable_entity_resource: BaseCollectableEntity)
 
+
+func emit_collect_entity(collectable_entity_resource: BaseCollectableResource):
+	collect_entity.emit(collectable_entity_resource)
 
