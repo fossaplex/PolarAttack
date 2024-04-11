c58d9f1e6b053c0f07b5e9d97696647e5a7f463d
Mark <marklmiller2010@gmail.com>
2024-04-10 21:42:27 -0400

    Made progress bar an inheritable class.

--- Scenes/ProgressBar/ProgressBar.gd
+++ Scenes/ProgressBar/ProgressBar.gd
@@ -1,28 +1,22 @@
 extends TextureProgressBar
+class_name Generic_Progress_Bar
 
-# Additional properties
-var increment_amount = 10 # Default increment amount
-var max_value_scaler = 1.12 # Max value scale coefficient
 var progress_color = Color(0.0, 1.0, 0.0, 1.0) # Default color (Green)
 
 func _ready():
-	# Example usage of setters on ready
-	value = 0
-	max_value = 100 # Setting a default max value
-	#set_progress_color(progress_color) # Applying the default color
+	pass
 
 func _process(delta):
 	pass
 
 # Increment function
-func increment():
+func increment(increment_amount):
 	value += increment_amount
 	if value > max_value:
-		value = value - max_value
-		max_value = max_value * max_value_scaler
+		value = max_value
 
-func decrement():
-	value -= increment_amount
+func decrement(decrement_amount):
+	value -= decrement_amount
 	if value < min_value:
 		value = 0
 
@@ -33,13 +27,6 @@ func get_current_value():
 func set_current_value(new_value):
 	value = new_value
 
-# Increment Amount getters and setters
-func get_increment_amount():
-	return increment_amount
-
-func set_increment_amount(new_increment_amount):
-	increment_amount = new_increment_amount
-
 # Progress Color getters and setters
 func get_progress_color():
 	return progress_color
@@ -47,8 +34,3 @@ func get_progress_color():
 func set_progress_color(new_color):
 	progress_color = new_color
 	modulate = new_color
-
-
-func _on_timer_timeout():
-	increment()
-	pass # Replace with function body.
