4bd4802ca51a8e639d2a46adf05256fae1703b62
ekareem <emmanuel45kareem@gmail.com>
2024-04-16 00:46:40 -0400

    remove mob

--- Scenes/Spawner/Enemy Spawner.gd
+++ Scenes/Spawner/Enemy Spawner.gd
@@ -1,13 +1,9 @@
 extends Node2D
 var seal_scene = preload("res://Scenes/Enemies/Enemy 1/Seal.tscn")
-var moab_scene = preload("res://Scenes/Enemies/Enemy 2/Moab.tscn")
 
 func _ready():
 	$Timer.start()
 
 func _on_timer_timeout():
 	var enemy_seal: Node2D = seal_scene.instantiate()
-	#var enemy_moab = moab_scene.instantiate()
-	#var enemy_list = [enemy_seal, enemy_moab]
-	#var r = randi_range(0,enemy_list.size() - 1)
 	add_child(enemy_seal)
