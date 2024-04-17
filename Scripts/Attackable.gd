e8974fb1ce69e56015d8fd7c224549eb7a61a083
ekareem <emmanuel45kareem@gmail.com>
2024-04-16 20:56:10 -0400

    spelling

--- Scripts/Attackable.gd
+++ Scripts/Attackable.gd
@@ -1,25 +1,25 @@
 class_name Attackable
 extends Node
 
-signal on_damage_dealt(charachter: CharacterBase, damage: float)
+signal on_damage_dealt(character: CharacterBase, damage: float)
 
 @export var damage := 10.0
 
-func deal_damange(charachter: CharacterBase) -> void:
-	var previous_health = charachter.health
-	charachter.health = max(0 , charachter.health - damage)
-	if previous_health != charachter.health:
-		on_damage_dealt.emit(charachter, damage)
+func deal_damange(character: CharacterBase) -> void:
+	var previous_health = character.health
+	character.health = max(0 , character.health - damage)
+	if previous_health != character.health:
+		on_damage_dealt.emit(character, damage)
 
-func deal_damange_delta(charachter: CharacterBase, delta: float) -> void:
+func deal_damange_delta(character: CharacterBase, delta: float) -> void:
 	var damage_this_frame = damage * delta
-	var previous_health = charachter.health
-	charachter.health = max(0 , charachter.health - damage_this_frame)
-	if previous_health != charachter.health:
-		on_damage_dealt.emit(charachter, damage)
+	var previous_health = character.health
+	character.health = max(0 , character.health - damage_this_frame)
+	if previous_health != character.health:
+		on_damage_dealt.emit(character, damage)
 
-func deal_damange_override(charachter: CharacterBase, new_damage: float) -> void:
-	var previous_health = charachter.health
-	charachter.health = max(0 , charachter.health - new_damage)
-	if previous_health != charachter.health:
-		on_damage_dealt.emit(charachter, damage)
+func deal_damange_override(character: CharacterBase, new_damage: float) -> void:
+	var previous_health = character.health
+	character.health = max(0 , character.health - new_damage)
+	if previous_health != character.health:
+		on_damage_dealt.emit(character, damage)
