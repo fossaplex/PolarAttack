23af0ebdac505c039d23bcaf7b10bcb172348490
PBHotdog <pbpetunia@gmail.com>
2024-05-15 19:03:45 -0400

    Finalize Main Menu Changes

--- Scenes/Player/DeathState.gd
+++ Scenes/Player/DeathState.gd
@@ -4,7 +4,7 @@ extends State
 @onready var animation_player := $"../../AnimationPlayer" as AnimationPlayer
 @onready var sprite := $"../../Sprite2D" as Sprite2D
 @onready var player := $"../.." as Player
-
+@onready var GAME_OVER_SCENE := preload("res://Scenes/GameOver/game_over_scene.tscn") as PackedScene
 
 signal is_dead
 
@@ -12,6 +12,7 @@ func enter() -> void:
 	super()
 	animation_player.play("death")
 	is_dead.emit()
+	SceneTransition.change_scene(GAME_OVER_SCENE, "packed")
 
 func process_physics(delta: float) -> void:
 	super(delta)
